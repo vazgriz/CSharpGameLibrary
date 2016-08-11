7329a873fb6efb2724a5b7ab287157ab6d66a598
rhynodegreat <rhynodegreat@gmail.com>
2016-08-10 22:57:45 -0500

    Added ShaderModule class

---
+++ CSharpGameLibrary/Vulkan/Managed/ShaderModule.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan.Unmanaged;
+
+namespace CSGL.Vulkan.Managed {
+    public class ShaderModuleCreateInfo {
+        public byte[] Data;
+
+        public ShaderModuleCreateInfo(byte[] data) {
+            Data = data;
+        }
+    }
+
+    public class ShaderModule : IDisposable {
+        VkShaderModule shaderModule;
+        bool disposed = false;
+
+        Device device;
+
+        vkCreateShaderModuleDelegate createShaderModule;
+        vkDestroyShaderModuleDelegate destroyShaderModule;
+
+        public ShaderModule(Device device, ShaderModuleCreateInfo info) {
+            if (device == null) throw new ArgumentNullException(nameof(device));
+            if (info == null) throw new ArgumentNullException(nameof(info));
+            if (info.Data == null) throw new ArgumentNullException(nameof(info.Data));
+
+            this.device = device;
+
+            createShaderModule = device.Commands.createShaderModule;
+            destroyShaderModule = device.Commands.destroyShaderModule;
+
+            CreateShader(info);
+        }
+
+        void CreateShader(ShaderModuleCreateInfo mInfo) {
+            unsafe
+            {
+                VkShaderModuleCreateInfo info = new VkShaderModuleCreateInfo();
+                info.sType = VkStructureType.StructureTypeShaderModuleCreateInfo;
+                info.codeSize = (ulong)mInfo.Data.LongLength;
+                
+                fixed (byte* ptr = mInfo.Data) {
+                    info.pCode = (uint*)ptr;
+                    var result = createShaderModule(device.Native, ref info, device.Instance.AllocationCallbacks, ref shaderModule);
+
+                    if (result != VkResult.Success) throw new ShaderModuleException(string.Format("Error creating shader module: {0}"));
+                }
+            }
+        }
+
+        public void Dispose() {
+            if (disposed) return;
+
+            unsafe
+            {
+                destroyShaderModule(device.Native, shaderModule, device.Instance.AllocationCallbacks);
+            }
+
+            disposed = true;
+        }
+    }
+
+    public class ShaderModuleException : Exception {
+        public ShaderModuleException(string message) : base(message) { }
+    }
+}
