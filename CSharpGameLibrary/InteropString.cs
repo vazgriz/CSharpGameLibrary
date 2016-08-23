045cd3269ec17e8e34bc305397fa0ac669e422a9
rhynodegreat <rhynodegreat@gmail.com>
2016-08-23 12:31:06 -0500

    Added InteropString class
    IDisposable wrapper that pins and gets the address of a given string

---
+++ CSharpGameLibrary/InteropString.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CSGL {
+    public class InteropString : IDisposable {
+        byte[] str;
+        bool disposed = false;
+        GCHandle handle;
+
+        public InteropString(string s) {
+            str = Interop.GetUTF8(s);
+            Init();
+        }
+
+        public InteropString(byte[] s) {
+            str = s;
+            Init();
+        }
+
+        void Init() {
+            handle = GCHandle.Alloc(str, GCHandleType.Pinned);
+        }
+
+        public IntPtr Address {
+            get {
+                return handle.AddrOfPinnedObject();
+            }
+        }
+
+        public int Length {
+            get {
+                return str.Length;
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool disposing) {
+            if (disposed) return;
+
+            handle.Free();
+
+            if (disposing) {
+                str = null;
+            }
+
+            disposed = true;
+        }
+
+        ~InteropString() {
+            Dispose(false);
+        }
+    }
+}
