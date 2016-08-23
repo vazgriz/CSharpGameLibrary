1e166abef5802bcc303a9e36ae391115e1660c87
rhynodegreat <rhynodegreat@gmail.com>
2016-08-23 13:04:43 -0500

    Added Marshalled<T> and PinnedArray<T> classes
    More interop helpers

---
+++ CSharpGameLibrary/Marshalled.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CSGL {
+    public class Marshalled<T> : IDisposable where T : struct {
+        IntPtr ptr;
+        bool disposed = false;
+        static int elementSize;
+
+        static Marshalled() {
+            elementSize = Marshal.SizeOf<T>();
+        }
+
+        public Marshalled() {
+            Init();
+        }
+
+        public Marshalled(T init) {
+            Init();
+            Marshal.StructureToPtr<T>(init, ptr, false);
+        }
+
+        void Init() {
+            ptr = Marshal.AllocHGlobal(elementSize);
+        }
+
+        public IntPtr Address {
+            get {
+                return ptr;
+            }
+        }
+
+        public T Value {
+            get {
+                return Marshal.PtrToStructure<T>(ptr);
+            }
+            set {
+                Marshal.StructureToPtr(value, ptr, true);
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool disposing) {
+            if (disposed) return;
+
+            Marshal.DestroyStructure<T>(ptr);
+            Marshal.FreeHGlobal(ptr);
+
+            disposed = true;
+        }
+
+        ~Marshalled() {
+            Dispose(false);
+        }
+    }
+}
---
+++ CSharpGameLibrary/PinnedArray.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CSGL {
+    public class PinnedArray<T> : IDisposable where T : struct {
+        T[] array;
+        GCHandle handle;
+        bool disposed = false;
+        static int elementSize;
+
+        static PinnedArray() {
+            elementSize = Marshal.SizeOf<T>();
+        }
+
+        public PinnedArray(T[] array) {
+            this.array = array;
+            Init();
+        }
+
+        public PinnedArray(int count) {
+            if (count < 0) throw new ArgumentOutOfRangeException(nameof(count), "Count must be positive");
+            array = new T[count];
+            Init();
+        }
+
+        void Init() {
+            handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+        }
+
+        public IntPtr Address {
+            get {
+                return handle.AddrOfPinnedObject();
+            }
+        }
+
+        public int Length {
+            get {
+                return array.Length;
+            }
+        }
+
+        public T this[int i] {
+            get {
+                return array[i];
+            }
+            set {
+                array[i] = value;
+            }
+        }
+
+        public IntPtr AddressOf(int i) {
+            return Address + (i * elementSize);
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool diposing) {
+            if (disposed) return;
+
+            handle.Free();
+
+            if (diposing) {
+                array = null;
+            }
+
+            disposed = true;
+        }
+
+        ~PinnedArray() {
+            Dispose(false);
+        }
+    }
+}
