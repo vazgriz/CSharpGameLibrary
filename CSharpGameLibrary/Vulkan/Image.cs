4523db91d0721c24038e5e3aed736cf00e55e112
rhynodegreat <rhynodegreat@gmail.com>
2016-11-05 18:38:44 -0500

    Added public constructor to Image class

--- CSharpGameLibrary/Vulkan/Image.cs
+++ CSharpGameLibrary/Vulkan/Image.cs
@@ -3,7 +3,18 @@ using System.Collections.Generic;
 
 namespace CSGL.Vulkan {
     public class ImageCreateInfo {
-
+        public VkImageCreateFlags flags;
+        public VkImageType imageType;
+        public VkFormat format;
+        public VkExtent3D extent;
+        public uint mipLevels;
+        public uint arrayLayers;
+        public VkSampleCountFlags samples;
+        public VkImageTiling tiling;
+        public VkImageUsageFlags usage;
+        public VkSharingMode sharingMode;
+        public uint[] queueFamilyIndices;
+        public VkImageLayout initialLayout;
     }
 
     public class Image : IDisposable, INative<VkImage> {
@@ -23,14 +34,47 @@ namespace CSGL.Vulkan {
             this.image = image;
         }
 
-        //public Image(Device device, ImageCreateInfo info) {
-        //    Device = device;
-        //}
+        public Image(Device device, ImageCreateInfo info) {
+            if (device == null) throw new ArgumentNullException(nameof(device));
+            Device = device;
+        }
+
+        void CreateImage(ImageCreateInfo mInfo) {
+            var info = new VkImageCreateInfo();
+            info.sType = VkStructureType.StructureTypeImageCreateInfo;
+            info.flags = mInfo.flags;
+            info.imageType = mInfo.imageType;
+            info.format = mInfo.format;
+            info.extent = mInfo.extent;
+            info.mipLevels = mInfo.mipLevels;
+            info.arrayLayers = mInfo.arrayLayers;
+            info.samples = mInfo.samples;
+            info.tiling = mInfo.tiling;
+            info.usage = mInfo.usage;
+            info.sharingMode = mInfo.sharingMode;
+
+            var indicesMarshalled = new PinnedArray<uint>(mInfo.queueFamilyIndices);
+            info.queueFamilyIndexCount = (uint)indicesMarshalled.Length;
+            info.pQueueFamilyIndices = indicesMarshalled.Address;
+            info.initialLayout = mInfo.initialLayout;
+
+            try {
+                var result = Device.Commands.createImage(Device.Native, ref info, Device.Instance.AllocationCallbacks, out image);
+                if (result != VkResult.Success) throw new ImageException(string.Format("Error creating image: {0}", result));
+            }
+            finally {
+                indicesMarshalled.Dispose();
+            }
+        }
 
         public void Dispose() {
             if (disposed) return;
 
             disposed = true;
         }
+    }
+
+    public class ImageException : Exception {
+        public ImageException(string message) : base(message) { }
     }
 }
