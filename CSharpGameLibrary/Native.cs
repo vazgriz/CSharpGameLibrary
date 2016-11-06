cf8bf5db05f8ba35f4c1e7b4a20930c209660e5a
rhynodegreat <rhynodegreat@gmail.com>
2016-11-05 20:54:56 -0500

    Added Native and NativeArray class
    Performs the same function as Marshalled and MarshalledArray using
    System.Runtime.CompilerServices.Unsafe class. Much faster, but only
    works on blittable types.

---
+++ CSharpGameLibrary/Native.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace CSGL {
+    public class Native<T> : IDisposable where T : struct {
+        unsafe void* ptr;
+        bool disposed = false;
+        static int elementSize;
+        bool allocated = false;
+
+        static Native() {
+            elementSize = Unsafe.SizeOf<T>();
+        }
+
+        public Native() {
+            Allocate();
+        }
+
+        public Native(T value) {
+            Allocate();
+            Value = value;
+        }
+
+        public unsafe Native(void* ptr) {
+            if (ptr == null) throw new ArgumentNullException(nameof(ptr));
+
+            this.ptr = ptr;
+        }
+
+        public unsafe Native(void* ptr, T value) {
+            if (ptr == null) throw new ArgumentNullException(nameof(ptr));
+
+            this.ptr = ptr;
+            Value = value;
+        }
+
+        unsafe void Allocate() {
+            ptr = (void*)Marshal.AllocHGlobal(elementSize);
+            allocated = true;
+        }
+
+        public IntPtr Address {
+            get {
+                unsafe
+                {
+                    return (IntPtr)ptr;
+                }
+            }
+        }
+
+        public T Value {
+            get {
+                unsafe
+                {
+                    return Unsafe.Read<T>(ptr);
+                }
+            }
+            set {
+                unsafe
+                {
+                    Unsafe.Write<T>(ptr, value);
+                }
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+        }
+
+        void Dispose(bool disposing) {
+            if (disposed) return;
+
+            if (allocated) {
+                unsafe
+                {
+                    Marshal.FreeHGlobal((IntPtr)ptr);
+                }
+            }
+
+            disposed = true;
+        }
+
+        ~Native() {
+            Dispose(false);
+        }
+    }
+}
---
+++ CSharpGameLibrary/NativeArray.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace CSGL {
+    public class NativeArray<T> : IDisposable where T : struct {
+        int count;
+        unsafe void* ptr;
+        bool disposed = false;
+        bool allocated = false;
+        static int elementSize;
+
+        static NativeArray() {
+            elementSize = Unsafe.SizeOf<T>();
+        }
+
+        public NativeArray(int count) {
+            this.count = count;
+            Allocate(count);
+        }
+
+        public NativeArray(uint count) : this((int)count) { }
+
+        public NativeArray(T[] array) {
+            if (array != null) {
+                count = array.Length;
+                Allocate(array.Length);
+                unsafe
+                {
+                    for (int i = 0; i < count; i++) {
+                        Interop.Copy(array, ptr);
+                    }
+                }
+            }
+        }
+
+        public NativeArray(List<T> list) {
+            if (list != null) {
+                count = list.Count;
+                Allocate(count);
+                unsafe
+                {
+                    for (int i = 0; i < count; i++) {
+                        Unsafe.Write(GetAddressInternal(i), list[i]);
+                    }
+                }
+            }
+        }
+
+        public NativeArray(INative<T>[] array) {
+            if (array != null) {
+                count = array.Length;
+                Allocate(count);
+                unsafe
+                {
+                    for (int i = 0; i < count; i++) {
+                        Unsafe.Write(GetAddressInternal(i), array[i].Native);
+                    }
+                }
+            }
+        }
+
+        public unsafe NativeArray(void* ptr, T[] array) {
+            if (ptr == null) throw new ArgumentNullException(nameof(ptr));
+
+            if (array != null) {
+                count = array.Length;
+                this.ptr = ptr;
+
+                Interop.Copy(array, ptr);
+            }
+        }
+        
+        void Allocate(int count) {
+            unsafe
+            {
+                if (count > 0) ptr = (void*)Marshal.AllocHGlobal(count * elementSize);
+            }
+            allocated = true;
+        }
+
+        public T this[int i] {
+            get {
+                if (i < 0 || i >= count) throw new IndexOutOfRangeException(string.Format("Index {0} is out of range [0, {1}]", i, count));
+                unsafe
+                {
+                    return Unsafe.Read<T>(GetAddressInternal(i));
+                }
+            }
+            set {
+                if (i < 0 || i >= count) throw new IndexOutOfRangeException(string.Format("Index {0} is out of range [0, {1}]", i, count));
+                unsafe
+                {
+                    Unsafe.Write(GetAddressInternal(i), value);
+                }
+            }
+        }
+
+        public IntPtr Address {
+            get {
+                unsafe
+                {
+                    return (IntPtr)ptr;
+                }
+            }
+        }
+
+        unsafe void* GetAddressInternal(int i) {
+            return (void*)((byte*)ptr + (i * elementSize));
+        }
+
+        public IntPtr GetAddress(int i) {
+            unsafe
+            {
+                return (IntPtr)GetAddressInternal(i);
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+        }
+
+        void Dispose(bool disposing) {
+            if (disposed) return;
+
+            if (allocated) {
+                unsafe
+                {
+                    Marshal.FreeHGlobal((IntPtr)ptr);
+                }
+            }
+
+            disposed = true;
+        }
+
+        ~NativeArray() {
+            Dispose(false);
+        }
+    }
+}
