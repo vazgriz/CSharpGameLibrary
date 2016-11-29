d524261b6f46c277d3aeb918a33e3d21acfdaca3
rhynodegreat <rhynodegreat@gmail.com>
2016-11-28 21:12:35 -0600

    Added bindings for stb.dll

---
+++ STB/STB_native.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using CSGL;
+
+namespace STB.Unmanaged {
+    class STB_native {
+        const string lib = "stb";
+
+        [DllImport(lib)]
+        public static extern IntPtr stbi_load_from_memory(IntPtr buffer, int length,
+            ref int x, ref int y,
+            ref int comp, int req_comp,
+            out IntPtr error);
+
+        [DllImport(lib)]
+        public static extern IntPtr stbi_loadf_from_memory(IntPtr buffer, int length,
+            ref int x, ref int y,
+            ref int comp, int req_comp,
+            out IntPtr error);
+
+        [DllImport(lib)]
+        public static extern void stbi_hdr_to_ldr_gamma(float gamma);
+
+        [DllImport(lib)]
+        public static extern void stbi_hdr_to_ldr_scale(float scale);
+
+        [DllImport(lib)]
+        public static extern void stbi_ldr_to_hdr_gamma(float gamma);
+
+        [DllImport(lib)]
+        public static extern void stbi_ldr_to_hdr_scale(float scale);
+
+        [DllImport(lib)]
+        public static extern bool stbi_is_hdr_from_memory(IntPtr buffer, int length);
+
+        [DllImport(lib)]
+        public static extern bool stbi_is_hdr(byte[] fileName);
+
+        [DllImport(lib)]
+        public static extern void stbi_image_free(IntPtr image);
+
+        [DllImport(lib)]
+        public static extern int stbi_info_from_memory(IntPtr buffer, int len,
+            ref int x, ref int y,
+            ref int comp,
+            ref IntPtr error);
+
+        [DllImport(lib)]
+        public static extern void stbi_set_unpremultiply_on_load(int flag);
+
+        [DllImport(lib)]
+        public static extern void stbi_convert_iphone_png_to_rgb(int flag);
+
+        [DllImport(lib)]
+        public static extern void stbi_set_flip_vertically_on_load(int flag);
+    }
+}
