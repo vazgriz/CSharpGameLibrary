6dbc01a54b3f07895e96e98717344249c4e4c44d
rhynodegreat <rhynodegreat@gmail.com>
2017-02-10 14:24:37 -0600

    Add SignedDistance

---
+++ MSDFGen/SignedDistance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MSDFGen {
+    public struct SignedDistance {
+        public double distance;
+        public double dot;
+
+        public static SignedDistance Infinite { get; } = new SignedDistance(-1e240, 1);
+
+        public SignedDistance(double distance, double dot) {
+            this.distance = distance;
+            this.dot = dot;
+        }
+
+        public static bool operator < (SignedDistance a, SignedDistance b) {
+            return Math.Abs(a.distance) < Math.Abs(b.distance) ||
+                (Math.Abs(a.distance) == Math.Abs(b.distance) && a.dot < b.dot);
+        }
+
+        public static bool operator > (SignedDistance a, SignedDistance b) {
+            return Math.Abs(a.distance) > Math.Abs(b.distance) ||
+                (Math.Abs(a.distance) == Math.Abs(b.distance) && a.dot > b.dot);
+        }
+
+        public static bool operator <= (SignedDistance a, SignedDistance b) {
+            return Math.Abs(a.distance) < Math.Abs(b.distance) ||
+                (Math.Abs(a.distance) == Math.Abs(b.distance) && a.dot <= b.dot);
+        }
+
+        public static bool operator >= (SignedDistance a, SignedDistance b) {
+            return Math.Abs(a.distance) > Math.Abs(b.distance) ||
+                (Math.Abs(a.distance) == Math.Abs(b.distance) && a.dot >= b.dot);
+        }
+    }
+}
