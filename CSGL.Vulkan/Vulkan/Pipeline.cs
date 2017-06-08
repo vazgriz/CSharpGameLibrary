368e75a2b6513023f46ad46f72a6543f02664808
rhynodegreat <rhynodegreat@gmail.com>
2017-06-07 19:17:20 -0500

    Refactor Pipeline
    Make Pipeline abstract, Create new GraphicsPipeline and ComputePipeline classes that inherit from Pipeline.

--- CSGL.Vulkan/Vulkan/Pipeline.cs
+++ CSGL.Vulkan/Vulkan/Pipeline.cs
@@ -4,38 +4,11 @@ using System.Collections.Generic;
 using CSGL.Vulkan.Unmanaged;
 
 namespace CSGL.Vulkan {
-    public class GraphicsPipelineCreateInfo {
-        public VkPipelineCreateFlags flags;
-        public List<PipelineShaderStageCreateInfo> stages;
-        public PipelineVertexInputStateCreateInfo vertexInputState;
-        public PipelineInputAssemblyStateCreateInfo inputAssemblyState;
-        public PipelineTessellationStateCreateInfo tessellationState;
-        public PipelineViewportStateCreateInfo viewportState;
-        public PipelineRasterizationStateCreateInfo rasterizationState;
-        public PipelineMultisampleStateCreateInfo multisampleState;
-        public PipelineDepthStencilStateCreateInfo depthStencilState;
-        public PipelineColorBlendStateCreateInfo colorBlendState;
-        public PipelineDynamicStateCreateInfo dynamicState;
-        public PipelineLayout layout;
-        public RenderPass renderPass;
-        public uint subpass;
-        public Pipeline basePipelineHandle;
-        public int basePipelineIndex;
-    }
-
-    public class ComputePipelineCreateInfo {
-        public VkPipelineCreateFlags flags;
-        public PipelineShaderStageCreateInfo stage;
-        public PipelineLayout layout;
-        public Pipeline basePipelineHandle;
-        public int basePipelineIndex;
-    }
-
-    public class Pipeline : IDisposable, INative<VkPipeline> {
-        VkPipeline pipeline;
+    public abstract class Pipeline : IDisposable, INative<VkPipeline> {
+        protected VkPipeline pipeline;
         bool disposed = false;
 
-        public Device Device { get; private set; }
+        public Device Device { get; protected set; }
 
         public VkPipeline Native {
             get {
@@ -43,155 +16,7 @@ namespace CSGL.Vulkan {
             }
         }
 
-        internal Pipeline(Device device, VkPipeline pipeline) {
-            Device = device;
-            this.pipeline = pipeline;
-        }
-
-        public Pipeline(Device device, GraphicsPipelineCreateInfo info, PipelineCache cache) {
-            if (device == null) throw new ArgumentNullException(nameof(device));
-            if (info == null) throw new ArgumentNullException(nameof(info));
-
-            Device = device;
-
-            VkPipelineCache nativeCache = VkPipelineCache.Null;
-            if (cache != null) {
-                nativeCache = cache.Native;
-            }
-
-            pipeline = CreatePipelinesInternal(device, new GraphicsPipelineCreateInfo[] { info }, nativeCache)[0];
-        }
-
-        //graphics
-        public static Pipeline[] CreatePipelines(Device device, GraphicsPipelineCreateInfo[] infos, PipelineCache cache) {
-            if (device == null) throw new ArgumentNullException(nameof(device));
-            if (infos == null) throw new ArgumentNullException(nameof(infos));
-            for (int i = 0; i < infos.Length; i++) {
-                if (infos[i] == null) throw new ArgumentNullException(string.Format("Element {0} of {1} is null", i, nameof(infos)));
-            }
-
-            VkPipelineCache nativeCache = VkPipelineCache.Null;
-            if (cache != null) {
-                nativeCache = cache.Native;
-            }
-
-            var pipelines = new Pipeline[infos.Length];
-            var natives = CreatePipelinesInternal(device, infos, nativeCache);
-
-            for (int i = 0; i < infos.Length; i++) {
-                pipelines[i] = new Pipeline(device, natives[i]);
-            }
-
-            return pipelines;
-        }
-
-        static VkPipeline[] CreatePipelinesInternal(Device device, GraphicsPipelineCreateInfo[] mInfos, VkPipelineCache cache) {
-            int count = mInfos.Length;
-            var infosMarshalled = new MarshalledArray<VkGraphicsPipelineCreateInfo>(count);
-            var pipelineResults = new VkPipeline[count];
-            var marshalledArrays = new DisposableList<IDisposable>(count);
-
-            for (int i = 0; i < count; i++) {
-                VkGraphicsPipelineCreateInfo info = new VkGraphicsPipelineCreateInfo();
-                var mInfo = mInfos[i];
-
-                info.sType = VkStructureType.GraphicsPipelineCreateInfo;
-                info.flags = mInfo.flags;
-
-                int stagesCount = mInfo.stages.Count;
-                var stagesMarshalled = new MarshalledArray<VkPipelineShaderStageCreateInfo>(stagesCount);
-                for (int j = 0; j < stagesCount; j++) {
-                    stagesMarshalled[j] = mInfo.stages[j].GetNative(marshalledArrays);
-                }
-
-                info.stageCount = (uint)stagesCount;
-                info.pStages = stagesMarshalled.Address;
-
-                marshalledArrays.Add(stagesMarshalled);
-
-                if (mInfo.vertexInputState != null) {
-                    var m = new Marshalled<VkPipelineVertexInputStateCreateInfo>(mInfo.vertexInputState.GetNative(marshalledArrays));
-                    info.pVertexInputState = m.Address;
-                    marshalledArrays.Add(m);
-                }
-
-                if (mInfo.inputAssemblyState != null) {
-                    var m = new Marshalled<VkPipelineInputAssemblyStateCreateInfo>(mInfo.inputAssemblyState.GetNative());
-                    info.pInputAssemblyState = m.Address;
-                    marshalledArrays.Add(m);
-                }
-
-                if (mInfo.tessellationState != null) {
-                    var m = new Marshalled<VkPipelineTessellationStateCreateInfo>(mInfo.tessellationState.GetNative());
-                    info.pTessellationState = m.Address;
-                    marshalledArrays.Add(m);
-                }
-
-                if (mInfo.viewportState != null) {
-                    var m = new Marshalled<VkPipelineViewportStateCreateInfo>(mInfo.viewportState.GetNative(marshalledArrays));
-                    info.pViewportState = m.Address;
-                }
-
-                if (mInfo.rasterizationState != null) {
-                    var m = new Marshalled<VkPipelineRasterizationStateCreateInfo>(mInfo.rasterizationState.GetNative());
-                    info.pRasterizationState = m.Address;
-                    marshalledArrays.Add(m);
-                }
-
-                if (mInfo.multisampleState != null) {
-                    var m = new Marshalled<VkPipelineMultisampleStateCreateInfo>(mInfo.multisampleState.GetNative());
-                    info.pMultisampleState = m.Address;
-                    marshalledArrays.Add(m);
-                }
-
-                if (mInfo.depthStencilState != null) {
-                    var m = new Marshalled<VkPipelineDepthStencilStateCreateInfo>(mInfo.depthStencilState.GetNative());
-                    info.pDepthStencilState = m.Address;
-                    marshalledArrays.Add(m);
-                }
-
-                if (mInfo.colorBlendState != null) {
-                    var m = new Marshalled<VkPipelineColorBlendStateCreateInfo>(mInfo.colorBlendState.GetNative(marshalledArrays));
-                    info.pColorBlendState = m.Address;
-                    marshalledArrays.Add(m);
-                }
-
-                if (mInfo.dynamicState != null) {
-                    var m = new Marshalled<VkPipelineDynamicStateCreateInfo>(mInfo.dynamicState.GetNative(marshalledArrays));
-                    info.pDynamicState = m.Address;
-                    marshalledArrays.Add(m);
-                }
-
-                info.layout = mInfo.layout.Native;
-
-                if (mInfo.renderPass != null) {
-                    info.renderPass = mInfo.renderPass.Native;
-                }
-
-                info.subpass = mInfo.subpass;
-                if (mInfo.basePipelineHandle != null) {
-                    info.basePipelineHandle = mInfo.basePipelineHandle.Native;
-                }
-                info.basePipelineIndex = mInfo.basePipelineIndex;
-
-                infosMarshalled[i] = info;
-            }
-
-            using (infosMarshalled)
-            using (marshalledArrays)
-            using (var pipelinesMarshalled = new PinnedArray<VkPipeline>(pipelineResults)) {
-                var result = device.Commands.createGraphicsPiplines(
-                    device.Native, cache,
-                    (uint)count, infosMarshalled.Address,
-                    device.Instance.AllocationCallbacks, pipelinesMarshalled.Address);
-
-                if (result != VkResult.Success) throw new PipelineException(string.Format("Error creating pipeline: {0}", result));
-                return pipelineResults;
-            }
-        }
-
-        //compute
-        public static Pipeline[] CreatePipelines(Device device, ComputePipelineCreateInfo[] infos, PipelineCache cache) {
+        public static GraphicsPipeline[] CreatePipelines(Device device, GraphicsPipelineCreateInfo[] infos, PipelineCache cache) {
             if (device == null) throw new ArgumentNullException(nameof(device));
             if (infos == null) throw new ArgumentNullException(nameof(infos));
 
@@ -200,52 +25,35 @@ namespace CSGL.Vulkan {
                 nativeCache = cache.Native;
             }
 
-            var pipelines = new Pipeline[infos.Length];
-            var natives = CreatePipelinesInternal(device, infos, nativeCache);
+            var pipelines = new GraphicsPipeline[infos.Length];
+            var natives = GraphicsPipeline.CreatePipelinesInternal(device, infos, nativeCache);
 
             for (int i = 0; i < infos.Length; i++) {
-                pipelines[i] = new Pipeline(device, natives[i]);
+                pipelines[i] = new GraphicsPipeline(device, natives[i]);
             }
 
             return pipelines;
         }
 
-        static VkPipeline[] CreatePipelinesInternal(Device device, ComputePipelineCreateInfo[] mInfos, VkPipelineCache cache) {
-            int count = mInfos.Length;
-            var infosMarshalled = new MarshalledArray<VkComputePipelineCreateInfo>(count);
-            var pipelineResults = new VkPipeline[count];
-            var marshalledArrays = new DisposableList<IDisposable>(count);
+        public static ComputePipeline[] CreatePipelines(Device device, ComputePipelineCreateInfo[] infos, PipelineCache cache) {
+            if (device == null) throw new ArgumentNullException(nameof(device));
+            if (infos == null) throw new ArgumentNullException(nameof(infos));
 
-            for (int i = 0; i < count; i ++) {
-                var mInfo = mInfos[i];
-                VkComputePipelineCreateInfo info = new VkComputePipelineCreateInfo();
-                info.sType = VkStructureType.ComputePipelineCreateInfo;
-                info.flags = mInfo.flags;
-
-                info.stage = mInfo.stage.GetNative(marshalledArrays);
-
-                info.layout = mInfo.layout.Native;
-                if (mInfo.basePipelineHandle != null) {
-                    info.basePipelineHandle = mInfo.basePipelineHandle.Native;
-                }
-                info.basePipelineIndex = mInfo.basePipelineIndex;
-
-                infosMarshalled[i] = info;
+            VkPipelineCache nativeCache = VkPipelineCache.Null;
+            if (cache != null) {
+                nativeCache = cache.Native;
             }
 
-            using (infosMarshalled)
-            using (marshalledArrays)
-            using (var pipelinesMarshalled = new PinnedArray<VkPipeline>(pipelineResults)) {
-                var result = device.Commands.createComputePipelines(
-                    device.Native, cache,
-                    (uint)count, infosMarshalled.Address,
-                    device.Instance.AllocationCallbacks, pipelinesMarshalled.Address);
+            var pipelines = new ComputePipeline[infos.Length];
+            var natives = ComputePipeline.CreatePipelinesInternal(device, infos, nativeCache);
 
-                if (result != VkResult.Success) throw new PipelineException(string.Format("Error creating pipeline: {0}", result));
-                return pipelineResults;
+            for (int i = 0; i < infos.Length; i++) {
+                pipelines[i] = new ComputePipeline(device, natives[i]);
             }
+
+            return pipelines;
         }
-
+        
         public void Dispose() {
             Dispose(true);
             GC.SuppressFinalize(this);
