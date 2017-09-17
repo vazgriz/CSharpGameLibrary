6e8c51e51ddf316c0d99b469d39358144dd49574
vazgriz <vazgriz@gmail.com>
2017-09-16 20:12:12 -0500

    Refactor DescriptorSetLayout creation
    Add DescriptorSetLayoutBinding class

--- CSGL.Vulkan/VkDescriptorSetLayout.cs
+++ CSGL.Vulkan/VkDescriptorSetLayout.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 
 namespace CSGL.Vulkan {
+    public class VkDescriptorSetLayoutBinding {
+        public int binding;
+        public VkDescriptorType descriptorType;
+        public int descriptorCount;
+        public VkShaderStageFlags stageFlags;
+        public IList<VkSampler> immutableSamplers;
+    }
+
     public class VkDescriptorSetLayoutCreateInfo {
-        public IList<Unmanaged.VkDescriptorSetLayoutBinding> bindings;
+        public IList<VkDescriptorSetLayoutBinding> bindings;
     }
 
     public class VkDescriptorSetLayout : IDisposable, INative<Unmanaged.VkDescriptorSetLayout> {
@@ -12,7 +20,7 @@ namespace CSGL.Vulkan {
         bool disposed;
 
         public VkDevice Device { get; private set; }
-        public IList<Unmanaged.VkDescriptorSetLayoutBinding> Bindings { get; private set; }
+        public IList<VkDescriptorSetLayoutBinding> Bindings { get; private set; }
 
         public Unmanaged.VkDescriptorSetLayout Native {
             get {
@@ -34,14 +42,44 @@ namespace CSGL.Vulkan {
         void CreateDescriptorSetLayout(VkDescriptorSetLayoutCreateInfo mInfo) {
             if (mInfo.bindings == null) throw new ArgumentNullException(nameof(mInfo.bindings));
 
-            var info = new Unmanaged.VkDescriptorSetLayoutCreateInfo();
-            info.sType = VkStructureType.DescriptorSetLayoutCreateInfo;
+            unsafe {
+                var info = new Unmanaged.VkDescriptorSetLayoutCreateInfo();
+                info.sType = VkStructureType.DescriptorSetLayoutCreateInfo;
 
-            var bindingsMarshalled = new MarshalledArray<Unmanaged.VkDescriptorSetLayoutBinding>(mInfo.bindings);
-            info.bindingCount = (uint)bindingsMarshalled.Count;
-            info.pBindings = bindingsMarshalled.Address;
+                int totalSamplerCount = 0;
+                for (int i = 0; i < mInfo.bindings.Count; i++) {
+                    if (mInfo.bindings[i].immutableSamplers != null) {
+                        totalSamplerCount += mInfo.bindings[i].descriptorCount;
+                    }
+                }
 
-            using (bindingsMarshalled) {
+                var samplersNative = stackalloc Unmanaged.VkSampler[totalSamplerCount];
+                int samplerIndex = 0;
+
+                var bindingsNative = stackalloc Unmanaged.VkDescriptorSetLayoutBinding[mInfo.bindings.Count];
+
+                for (int i = 0; i < mInfo.bindings.Count; i++) {
+                    var binding = mInfo.bindings[i];
+                    var bindingNative = new Unmanaged.VkDescriptorSetLayoutBinding();
+                    bindingNative.binding = (uint)binding.binding;
+                    bindingNative.descriptorCount = (uint)binding.descriptorCount;
+                    bindingNative.descriptorType = binding.descriptorType;
+                    bindingNative.stageFlags = binding.stageFlags;
+
+                    if (binding.immutableSamplers != null) {
+                        for (int j = 0; j < binding.immutableSamplers.Count; j++) {
+                            samplersNative[samplerIndex + j] = binding.immutableSamplers[j].Native;
+                        }
+                        bindingNative.pImmutableSamplers = (IntPtr)(samplersNative + samplerIndex);
+                        samplerIndex += binding.immutableSamplers.Count;
+                    }
+
+                    bindingsNative[i] = bindingNative;
+                }
+
+                info.bindingCount = (uint)mInfo.bindings.Count;
+                info.pBindings = (IntPtr)bindingsNative;
+                
                 var result = Device.Commands.createDescriptorSetLayout(Device.Native, ref info, Device.Instance.AllocationCallbacks, out descriptorSetLayout);
                 if (result != VkResult.Success) throw new DescriptorSetLayoutException(result, string.Format("Error creating description set layout: {0}", result));
             }
