73bcfcc96b6db893b86becdd071081ab524b3c8f
vazgriz <vazgriz@gmail.com>
2017-09-17 14:06:56 -0500

    Refactor VK class

--- CSGL.Vulkan/Unmanaged/VK.cs
+++ CSGL.Vulkan/Unmanaged/VK.cs
@@ -17,10 +17,19 @@ namespace CSGL.Vulkan.Unmanaged {
             return name.Substring(0, name.Length - 8);  //"Delegate".Length == 8
         }
 
+        public static IntPtr Load(VkInstance instance, string commandName) {
+            var native = Interop.GetUTF8(commandName);
+            return GetInstanceProcAddr(instance, native);
+        }
+
+        public static IntPtr Load(VkDevice device, string commandName) {
+            var native = Interop.GetUTF8(commandName);
+            return GetDeviceProcAddr(device, native);
+        }
+
         public static void Load<T>(ref T del, VkInstance instance) {
-            var command = GetCommand<T>();
-            var native = Interop.GetUTF8(command);
-            IntPtr ptr = VK.GetInstanceProcAddr(instance, native);
+            var commandName = GetCommand<T>();
+            IntPtr ptr = Load(instance, commandName);
             del = Marshal.GetDelegateForFunctionPointer<T>(ptr);
         }
 
@@ -29,9 +38,8 @@ namespace CSGL.Vulkan.Unmanaged {
         }
 
         public static void Load<T>(ref T del, VkDevice device) {
-            var command = GetCommand<T>();
-            var native = Interop.GetUTF8(command);
-            IntPtr ptr = VK.GetDeviceProcAddr(device, native);
+            var commandName = GetCommand<T>();
+            IntPtr ptr = Load(device, commandName);
             del = Marshal.GetDelegateForFunctionPointer<T>(ptr);
         }
 
